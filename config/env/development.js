fd2ecdaa353c98a7d70ebfc6512342ab19eb25bd
Nanne Baars <Nanne Baars>
2017-06-09 11:31:26 +0200

    Moving develop to use localhost:8080 for ZAP

--- config/env/development.js
+++ config/env/development.js
@@ -11,8 +11,8 @@ module.exports = {
   //db: 'mongodb://<dbuser>:<dbpassword>@<databasename>',
 
   // If you want to debug regression tests, you will need the following which is also in the test config:
-  zapHostName: "192.168.1.195",
-  zapPort: "9000",
+  zapHostName: "localhost",
+  zapPort: "8080",
   // Required from Zap 2.4.1. This key is set in Zap Options -> API _Api Key.
   zapApiKey: "dvarh87o132g62dtdst0d5ide7",
   // Required if debugging security regression tests.
